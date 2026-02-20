a0b243345fe4b660859e8b83997771293499322e
kamal Kushwah <kamalkushwah206@gmail.com>
2026-02-20 19:21:25 +0530

    add console

--- routes/pool.js
+++ routes/pool.js
@@ -1,4 +1,10 @@
-var mysql=require('mysql2')
+var mysql = require('mysql2')
+
+console.log("HOST:", process.env.MYSQLHOST);
+console.log("PORT:", process.env.MYSQLPORT);
+console.log("USER:", process.env.MYSQLUSER);
+console.log("DB:", process.env.MYSQLDATABASE);
+
 const pool = mysql.createConnection({
   host: process.env.MYSQLHOST,
   port: process.env.MYSQLPORT,
@@ -6,9 +12,5 @@ const pool = mysql.createConnection({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
 });
+
 module.exports = pool;
-
-
-
-
-
