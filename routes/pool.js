197320f81fa50e45cbb7f1b4e1951109adb87756
kamal Kushwah <kamalkushwah206@gmail.com>
2026-02-20 18:10:40 +0530

    adding env variable

--- routes/pool.js
+++ routes/pool.js
@@ -1,12 +1,14 @@
 var mysql=require('mysql2')
-const db = mysql.createConnection({
-  host: mysql.railway.internal,
-  port: 3306,
-  user: root,
-  password: YJLXZjDsDSjuTkjCBoEbUzWpNoFLJRVk,
-  database: railway,
+const pool = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 module.exports = pool;
+
+
 
 
 
