f8299963c94e24092a3cbbb42dee6d59620cc5ba
kamal Kushwah <kamalkushwah206@gmail.com>
2026-02-20 17:45:43 +0530

    adding port

--- app.js
+++ app.js
@@ -60,4 +60,7 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 module.exports = app;
--- routes/pool.js
+++ routes/pool.js
@@ -1,11 +1,12 @@
-var mysql=require('mysql')
-var pool=mysql.createPool({
-host:"mysql.railway.internal",
-port:3306,
-user:"root",
-password:"YJLXZjDsDSjuTkjCBoEbUzWpNoFLJRVk",
-database:"railway,",
-multipleStatements:true,
-connectionLimit:100
-})
+var mysql=require('mysql2')
+const db = mysql.createConnection({
+  host: mysql.railway.internal,
+  port: 3306,
+  user: root,
+  password: YJLXZjDsDSjuTkjCBoEbUzWpNoFLJRVk,
+  database: railway,
+});
 module.exports = pool;
+
+
+
